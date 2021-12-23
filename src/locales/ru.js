0827a6cfeafeee6ef79e4fc498e0f04116fdc4ea
Ayzek <Ayzekmanduar@mail.ru>
2021-12-23 22:17:41 +0900

    111

---
+++ src/locales/ru.js
@@ -0,0 +1,8 @@
+export default {
+  translation: {
+    isValid: 'Rss успешно загружен',
+    errorUlr: 'Ссылка должна быть валидным URL',
+    invalidRss: 'Ресурс не содержит валидный RSS',
+    duplicateUrl: 'RSS уже существует',
+  },
+};
