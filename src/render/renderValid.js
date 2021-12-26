2893794c381caebade4ccd64df2c666bf7e87faa
Ayzek <Ayzekmanduar@mail.ru>
2021-12-26 12:49:53 +0900

    fixed bugs

--- src/render/renderValid.js
+++ src/render/renderValid.js
@@ -1,20 +1,23 @@
 import i18next from 'i18next';
 
-const renderValid = (state, value) => {
+const renderValid = (state, path, value) => {
+  console.log(state);
   const input = document.querySelector('input');
   const validateParagh = document.querySelector('.feedback');
-  if (value === 'valid') {
-    input.classList.remove('is-invalid');
-    input.value = '';
-    input.focus();
-    validateParagh.textContent = i18next.t('isValid');
-    validateParagh.classList.remove('text-danger');
-    validateParagh.classList.add('text-success');
-  } else {
+  if (path === 'form.error') {
     input.classList.add('is-invalid');
-    validateParagh.textContent = state.textError;
-    validateParagh.classList.remove('text-success');
+    validateParagh.textContent = value;
     validateParagh.classList.add('text-danger');
+  }
+  if (path === 'validateForm') {
+    if (value === 'is-valid') {
+      input.classList.remove('is-invalid');
+      input.value = '';
+      input.focus();
+      validateParagh.textContent = i18next.t('isValid');
+      validateParagh.classList.remove('text-danger');
+      validateParagh.classList.add('text-success');
+    }
   }
 };
 export default renderValid;
