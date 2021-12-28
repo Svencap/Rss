004c70c44bea8da7ac57fe734e333f4936cd0b63
Ayzek <Ayzekmanduar@mail.ru>
2021-12-28 21:44:44 +0900

    parse link for post

--- src/DOMparser.js
+++ src/DOMparser.js
@@ -1,5 +1,25 @@
+import { uniqueId } from 'lodash';
+
 const parserDom = (content) => {
+  const result = {
+    posts: [],
+    feeds: [],
+  };
   const parser = new DOMParser();
-  return parser.parseFromString(content, 'application/xml');
+  const doc = parser.parseFromString(content, 'application/xml');
+
+  const feedTitle = doc.querySelector('title').textContent;
+  const feedDescription = doc.querySelector('description').textContent;
+
+  const posts = Array.from(doc.querySelectorAll('item')).map((item) => {
+    const linkFeed = item.querySelector('link').textContent;
+    const titleFeed = item.querySelector('title').textContent;
+    const descriptionFeed = item.querySelector('description').textContent;
+    return { title: titleFeed, description: descriptionFeed, link: linkFeed };
+  });
+
+  result.posts = [...posts];
+  result.feeds.push({ id: uniqueId(), title: feedTitle, description: feedDescription });
+  return result;
 };
 export default parserDom;
