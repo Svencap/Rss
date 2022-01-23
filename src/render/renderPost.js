3912fcc12ad2c0de9ccc4846f316b99fb0dc9819
Ayzek <Ayzekmanduar@mail.ru>
2022-01-23 23:01:37 +0900

    refactored code

--- src/render/renderPost.js
+++ src/render/renderPost.js
@@ -3,7 +3,7 @@ const renderPost = (posts) => {
   const ul = document.createElement('ul');
   ul.className = ('class', 'list-group border-0 rounded-0');
 
-  const list = posts.map(({ title, link }) => `<li class="list-group-item d-flex justify-content-between align-items-start border-0 border-end-0"><a href=${link}>${title}</a><button type="button" class="btn btn-outline-primary btn-sm">Просмотр</button></li>`).join(' ');
+  const list = posts.map(({ title, link }) => `<li class="list-group-item d-flex justify-content-between align-items-start border-0 border-end-0"><a href=${link} target="_blank">${title}</a><button type="button" class="btn btn-outline-primary btn-sm">Просмотр</button></li>`).join(' ');
   ul.innerHTML = list;
   const h4Post = '<div class="card-body"><h2 class="card-title h4">Посты</h2></div>';
 
