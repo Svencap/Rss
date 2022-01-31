eaae3833b596f42d2cc9a53fc5dcd33488b9a38b
Ayzek <Ayzekmanduar@mail.ru>
2022-01-31 23:13:04 +0900

    added function which generates id for posts

--- src/generatedID.js
+++ src/generatedID.js
@@ -3,6 +3,7 @@ import { uniqueId } from 'lodash';
 const generatedId = (data) => {
   const result = data.map((item) => {
     if (!Object.prototype.hasOwnProperty.call(item, 'id')) {
+      // eslint-disable-next-line no-param-reassign
       item.id = uniqueId();
     }
     return item;
